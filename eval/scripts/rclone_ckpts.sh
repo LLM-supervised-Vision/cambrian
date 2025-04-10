7956051a2b3b084e7128e98df0252a4a30859e6a
Ziteng Wang <zw2526@nyu.edu>
2025-04-10 10:06:36 -0700

    added ckpt

--- eval/scripts/rclone_ckpts.sh
+++ eval/scripts/rclone_ckpts.sh
@@ -7,9 +7,11 @@ GDRIVE_NAME="gdrive"
 
 # List of MLLM keys
 MLLM_KEYS=(
+    1_sft_p2-3b-224_corrected-pp
+
     # 0_sft_p2-3b-224
     # 1_sft_p2-3b-224
-    2_sft_p2-3b-224
+    # 2_sft_p2-3b-224
 
     # 2_sft_p3b224_wd0.0_re-init
     # 2_sft_p3b224_wd0.0_inherit
